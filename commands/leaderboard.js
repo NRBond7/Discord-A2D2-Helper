4378b3163cea7f865b9e95db28f759dd8aa1d5f9
Noah <nrbond7@gmail.com>
2020-05-21 14:42:20 -0400

    Added skeleton for leaderboard commands.  Added handling for missing create param

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,30 +1,4 @@
-module.exports = {
-  name: '!leaderboard',
-  description: 'Manages leaderboards for anything following the format',
-  execute(msg, args) {
-    var admin = require("firebase-admin");
-    var db = admin.database();
-    var ref = db.ref("leaderboards");
-
-    if (args.length == 0) {
-      // todo: print off existing leaderboards
-      msg.channel.send(`Fetching leaderboards...`);
-      ref.once("value", function(snapshot) {
-        console.log(snapshot.val());
-        var leaderboards = [];
-        snapshot.forEach(function(childSnapshot) {
-          var key = childSnapshot.key;
-          leaderboards.push(childSnapshot.key);
-        })
-        msg.channel.send(`Here are the existing leaderboards:\n
-${leaderboards}
-          `);
-      });
-    } else if (args[0].toLowerCase() === 'help') {
-      // todo: send this to user in DM to prevent clutter?
-      // todo: add example to fetch leaderboard
-      msg.author.send(`
-Welcome to A2D2 Bot\'s leaderboard!\n\n
+const HELP_MESSAGE = `Welcome to A2D2 Bot\'s leaderboard!\n\n
 A leaderboard can be looked-up using:\n
 \"!leaderboard LEADERBOARD_NAME\"\n\n
 To create a leaderboard:\n
@@ -36,8 +10,48 @@ To add an entry to existing leaderboard:\n
 Example: \"!leaderboard addto PunsLeaderboard A2D2Bot 9000\"\n\n
 To remove an entry to existing leaderboard:\n
 \"!leaderboard removefrom LEADERBOARD_NAME USERNAME LEAADERBOARD_VALUE\"\n
-Example: \"!leaderboard removefrom PunsLeaderboard A2D2Bot 9000\"\n\n
-      `);
+Example: \"!leaderboard removefrom PunsLeaderboard A2D2Bot 9000\"\n\n`;
+
+module.exports = {
+  name: '!leaderboard',
+  description: 'Manages leaderboards for anything following the format',
+  execute(msg, args) {
+    var admin = require("firebase-admin");
+    var db = admin.database();
+    var ref = db.ref("leaderboards");
+
+    if (args.length == 0) {
+      msg.channel.send(`Fetching leaderboards...`);
+      ref.once("value", function(snapshot) {
+        console.log(snapshot.val());
+        var leaderboards = [];
+        snapshot.forEach(function(childSnapshot) {
+          var key = childSnapshot.key;
+          leaderboards.push(childSnapshot.key);
+        })
+        msg.channel.send(`Here are the existing leaderboards:\n${leaderboards}`);
+      });
+    } else if (args[0].toLowerCase() === 'help') {
+      msg.author.send(HELP_MESSAGE);
+    } else if (args[0].toLowerCase() === 'create') {
+      if  (typeof arg[1] == 'undefined') {
+        msg.channel.send(`Missing leaderboard name.  Invoke \"!leaderboard LEADERBOARD_NAME\"`);
+      }
+
+      // Check if leaderboard already exists
+      db.ref(`leaderboards/${arg1}`)
+      // Check if user passed in arg1 and it's properly formatted
+      // Create leaderboard
+      // report back
+    } else if (args[0].toLowerCase() === 'addTo') {
+      // check if leaderboard exists
+      // add entry
+    } else if (args[0].toLowerCase() === 'removeFrom') {
+      // check if leaderboard exists
+      // remove matching entry
+    } else {
+      // fetch leaderboard of name arg0
+      // return and sort entries
     }
 
     // arg0 command
