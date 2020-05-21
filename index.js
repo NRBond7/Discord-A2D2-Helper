7cb5bf46a23f22d70f600e111b8dbb6f6fb9cf9f
Noah <nrbond7@gmail.com>
2020-05-21 12:11:01 -0400

    Fixed missing curly brace

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const botCommands = require('./commands');
 // Your web app's Firebase configuration
 var admin = require("firebase-admin");
 admin.initializeApp({
-  credential: admin.credential.cert(
+  credential: admin.credential.cert({
     "private_key": process.env.FIREBASE_PRIVATE_KEY,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   }),
